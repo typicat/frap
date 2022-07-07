d0645fff82273457927d3bab7c297b156b34cb64
Karl Jonsson <typicat@users.noreply.github.com>
2022-07-07 14:31:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
- # ----------------------------------------------------------------------------
- # "THE BEER-WARE LICENSE" (Revision 42):
- # <kjonsson@gmail.com> wrote this file. As long as you retain this notice you
- # can do whatever you want with this stuff. If we meet some day, and you think
- # this stuff is worth it, you can buy me a beer in return
- # ----------------------------------------------------------------------------
- 
+# Copyright 2022 typicat
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 # needs
 from bs4 import BeautifulSoup
 import requests
@@ -25,7 +23,7 @@ if html.status_code != 200:
     html.raise_for_status()
 soup = BeautifulSoup(html.content, "html.parser")
 
-# table for displaying results
+# create table for displaying results
 table = Table(title="Fångstrapporter Kågeälven")
 
 table.add_column("Datum", style="dim")
